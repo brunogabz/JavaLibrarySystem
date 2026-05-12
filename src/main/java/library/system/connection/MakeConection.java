b95fa005d4f5714aaa8b67d9119988aca40aa8cf
BrunoGabriel <brunogabrielalbuquerque@gmail.com>
2026-05-12 02:25:40 -0300

    Update MakeConection.java

--- src/main/java/library/system/connection/MakeConection.java
+++ src/main/java/library/system/connection/MakeConection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class MakeConection {
     private static final String URL = "jdbc:postgresql://localhost:5432/library";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "1006";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
